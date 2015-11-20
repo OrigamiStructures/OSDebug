a7ed8175bf0563b5c07a0e3174fd7533a8f95137
Jason Tempestini <jason@curlymedia.com>
2015-11-20 12:38:19 -0800

    check for cake existance
    Fixes #1

--- src/Lib/OSDebug.php
+++ src/Lib/OSDebug.php
@@ -9,6 +9,14 @@
 
 if(!function_exists('osd')){
     function osd($param = NULL) {
+        if(class_exists('Cake\Core\App')){
+            var_dump("Hey, I'm in CAKE");
+        }
+//        $classes = get_declared_classes();
+//        foreach ($classes as $key => $class) {
+//            echo "<p>$class</p>";
+//        }
+//        var_dump(get_declared_classes());
         var_dump("Hey, I'm in osd!!");
         var_dump(__METHOD__);
     }
